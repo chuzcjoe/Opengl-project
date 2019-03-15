b2e27209b7362d1c8ca44c7687dfc4e6a3931e7a
Joe <44826941+chuzcjoe@users.noreply.github.com>
2019-03-15 02:39:47 -0400

    Add files via upload

---
+++ Camera.h
@@ -0,0 +1,41 @@
+#pragma once
+
+#include <GL\glew.h>
+
+#include <glm\glm.hpp>
+#include <glm\gtc\matrix_transform.hpp>
+
+#include <GLFW\glfw3.h>
+
+class Camera
+{
+public:
+	Camera();
+	Camera(glm::vec3 startPosition, glm::vec3 startUp, GLfloat startYaw, GLfloat startPitch, GLfloat startMoveSpeed, GLfloat startTurnSpeed);
+
+	void keyControl(bool* keys, GLfloat deltaTime);
+	void mouseControl(GLfloat xChange, GLfloat yChange);
+
+	glm::vec3 getCameraPosition();
+	glm::vec3 getCameraDirection();
+
+	glm::mat4 calculateViewMatrix();
+
+	~Camera();
+
+private:
+	glm::vec3 position;
+	glm::vec3 front;
+	glm::vec3 up;
+	glm::vec3 right;
+	glm::vec3 worldUp;
+
+	GLfloat yaw;
+	GLfloat pitch;
+
+	GLfloat moveSpeed;
+	GLfloat turnSpeed;
+
+	void update();
+};
+
